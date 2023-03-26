5548498f7d15baa862df798f6e48ca4c7b4ba26b
yuyaoowada <yuyaoowada@gmail.com>
2023-03-27 01:11:41 +0900

    bitflyer access

--- maiin.go
+++ maiin.go
@@ -2,14 +2,13 @@ package main
 
 import (
 	"fmt"
+	"gotrading/bitflyer"
 	"gotrading/config"
 	"gotrading/utils"
-	"log"
 )
 
 func main() {
 	utils.LoggingSettings(config.Config.LogFile)
-	log.Println("test")
-	fmt.Println(config.Config.ApiKey)
-	fmt.Println(config.Config.ApiSecret)
+	apiClient := bitflyer.New(config.Config.ApiKey, config.Config.ApiSecret)
+	fmt.Println(apiClient.GetBalance())
 }
